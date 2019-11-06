eb64fb99c991ed2018faee05ae295b2bf92772f1
Elena Bobadilla <m.elena.bobadilla@gmail.com>
2019-11-06 18:56:58 +0100

    Añadidos tests e2e
    Añadidos tests e2e: páginas home, region y country

--- practica1/apps/practica1-e2e/src/integration/app.spec.ts
+++ practica1/apps/practica1-e2e/src/integration/app.spec.ts
@@ -1,9 +1,67 @@
-import { getGreeting } from '../support/app.po';
+import { getGreeting, getTitle, waitLoadPage, getRegions, getContinents, getCountries, clickRegion, clickContinent } from '../support/app.po';
 
-describe('practica1', () => {
+describe('GIVEN: app practica1 - Home page', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to practica1!');
+  context('WHEN: user visits home page', () => {
+    it('THEN: should display welcome message', () => {
+      getGreeting().contains('Práctica 1');
+    });
+    it('THEN: should display title', () => {
+      getTitle().contains('Home page');
+    });
+    it('THEN: should display 7 regions', () => {
+      waitLoadPage();
+      getRegions().should('have.length', 7)
+    });
+    it('THEN: clicking region link navigates to region page', function() {
+      waitLoadPage();
+      clickRegion().url().should('include', '/region/EAS', { timeout: 10000 });
+    })
   });
 });
+
+describe('GIVEN: app practica1 - Region page', () => {
+  beforeEach(() => cy.visit('/region/ECS'));
+
+  context('WHEN: user visits region page', () => {
+    it('THEN: should display welcome message', () => {
+      getGreeting().contains('Práctica 1');
+    });
+    it('THEN: should display title', () => {
+      getTitle().contains('Página región continental');
+    });
+    it('THEN: should display 58 continents', () => {
+      waitLoadPage();
+      getContinents().should('have.length', 58)
+    });
+    it('THEN: clicking continent link navigates to region page', function() {
+      waitLoadPage();
+      clickContinent().url().should('include', '/country/ALB');
+    })
+  });
+
+});
+
+describe('GIVEN: app practica1 - Country page', () => {
+  beforeEach(() => cy.visit('/country/ESP'));
+
+  context('WHEN: user visits country page', () => {
+    it('THEN: should display welcome message', () => {
+      getGreeting().contains('Práctica 1');
+    });
+    it('THEN: should display title', () => {
+      getTitle().contains('Datos del país');
+    });
+    it('THEN: should display 1 country', () => {
+      waitLoadPage();
+      getCountries().should('have.length', 1)
+    });
+    it('THEN: clicking continent link navigates to region page', function() {
+      waitLoadPage();
+      clickRegion().url().should('include', '/region/ECS');
+    })
+  });
+
+});
+
--- practica1/apps/practica1-e2e/src/support/app.po.ts
+++ practica1/apps/practica1-e2e/src/support/app.po.ts
@@ -1,1 +1,10 @@
 export const getGreeting = () => cy.get('h1');
+export const getTitle = () => cy.get('h2');
+export const getRegions = () => cy.get('div.region');
+export const getContinents = () => cy.get('div.continent');
+export const getCountries = () => cy.get('div.country');
+export const clickRegion = () => cy.get('a.link_region:first').click();
+export const clickContinent = () => cy.get('a.link_continent:first').click();
+export const waitLoadPage = () => cy.wait(1000);
+
+
